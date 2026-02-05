6212c611a1e174ad93c6da6cb9ad27478dc1ad5b
Manohar4333 <thappetlamanohar@gmail.com>
2026-02-06 00:51:38 +0530

    two changes done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import Header from './components/Header'
 import WeatherCard from './components/WeatherCard'
 import Footer from './components/Footer'
-import Search from './components/search'
+import Search from './components/Search'
 
 const App = () => {
   const [weatherDetails,setWeatherDetails] = useState(null)
